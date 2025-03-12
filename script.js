c05dcc2458f8fdb0e18074dac691169b2c121b8f
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-03-12 15:32:56 +0800

    add this

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const backToTopButton = document.getElementById("backToTopBtn");
 
 // When the user scrolls down 100px from the top of the document, show the button
-window.onscroll = function() {
+window.onscroll = function () {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         backToTopButton.style.display = "block";
     } else {
@@ -11,9 +11,22 @@ window.onscroll = function() {
 };
 
 // When the user clicks the button, scroll to the top of the document
-backToTopButton.onclick = function() {
+backToTopButton.onclick = function () {
     window.scrollTo({
         top: 0,
         behavior: "smooth" // This makes the scroll action smooth
     });
 };
+
+
+const closebtn = document.getElementById("closeBtn");
+const modal = document.getElementById("modal");
+modal.style.display = "none";
+closebtn.addEventListener("click", () => {
+    modal.style.display = "none"
+})
+
+const openModal = document.getElementById("openModal");
+openModal.addEventListener("click", () => {
+    modal.style.display = "block";
+})
