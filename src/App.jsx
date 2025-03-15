2162ca95ec87632b836f1f6e34c29205e0c8b44d
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-03-15 13:28:14 +0800

    add

---
+++ src/App.jsx
@@ -0,0 +1,128 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import { data } from './data';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+
+function App() {
+  const [selectedIndex, setSelectedIndex] = useState(null);
+  const [showButton, setShowButton] = useState(false); // State to control button visibility
+
+  // Scroll event handler
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setShowButton(true); // Show button if scrolled more than 100px
+    } else {
+      setShowButton(false); // Hide button if less than 100px
+    }
+  };
+
+  // Hook to add scroll event listener when component mounts
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const renderBoldText = (text) => {
+    const regex = /\[([^\]]+)\]/g;
+    const parts = text.split(regex);
+
+    return parts.map((part, index) => {
+      if (index % 2 !== 0) {
+        return <span key={index} className="font-bold">{part}</span>;
+      }
+      return part;
+    });
+  };
+
+  const handleQuestionClick = (index) => {
+    setSelectedIndex(index);
+  };
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Box className='p-[2rem]'>
+      <h1 className='text-3xl'>FAQ Script Scenario in Facebook comments</h1>
+      <p>Click the link to go to the script on how to answer it</p>
+      <ol className='mb-2 text-2xl'>
+        {data.map((item, index) => (
+          <li key={index}>
+            <a
+              className='text-blue-600 underline'
+              href={'#card' + index}
+              onClick={() => handleQuestionClick(index)}
+            >
+              {index + 1}. {item.question}
+            </a>
+          </li>
+        ))}
+      </ol>
+      <Box>
+        {data.map((item, index) => (
+          <Box key={index} id={'card' + index} className='bg-gray-50 mb-2'>
+            <h2
+              className={`p-[10px] ${selectedIndex === index ? 'bg-green-300 text-black' : 'bg-blue-900 text-white'}`}
+            >
+              {item.question}
+            </h2>
+            <Box className='p-2 text-black'>
+              <p className='text-lg mb-2'>
+                {renderBoldText(item.paragraph)}
+              </p>
+              {item.enroll && (
+                <p className="text-red-500 font-bold">{item.enroll}</p>
+              )}
+              {item.steps && (
+                <ul>
+                  {item.steps.map((step, stepIndex) => (
+                    <li key={stepIndex}>{step}</li>
+                  ))}
+                </ul>
+              )}
+              {item.cta && <p>{item.cta}</p>}
+              {item.alt && <p>{item.alt}</p>}
+            </Box>
+          </Box>
+        ))}
+        
+        {/* Back to Top Button */}
+        {showButton && (
+          <div className='bg-blue-900 fixed bottom-4 right-4'>
+            <Button 
+              sx={{ color: 'white' }}
+              onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+            >
+              Back to Top
+            </Button>
+          </div>
+        )}
+
+        <div className='bg-green-900 fixed bottom-4 left-4'>
+          <Button sx={{ color: 'white' }} onClick={() => setOpen(true)}>Open Message Script</Button>
+        </div>
+
+        <Dialog open={!!open}>
+          <DialogTitle>Script messenger</DialogTitle>
+          <DialogContent>
+            <h2>Could you kindly provide the following information for data purposes?</h2>
+            <ul>
+              <li>Full Name:</li>
+              <li>Email Address:</li>
+              <li>Contact Number:</li>
+              <li>Course Inquiry: GT Foundation</li>
+            </ul>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpen(false)} color='primary' variant='contained'>Close</Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/data/index.js
@@ -0,0 +1,91 @@
+export const data = [
+    {
+        question:"How to enroll in this course in this short course?",
+        paragraph:"Good day [Name of the Person]! To enroll in any of our short courses, please submit one (1) valid ID and fill out the application form available at the registrar's office.",
+        enroll:"To enroll in our short courses online, please visit the following link: https://courselist.mfi.org.ph/courses/55 (Please change the url base on the course choose)",
+        steps:[
+            "1.) Upon selecting your desired course, kindly scroll down to view the available schedules.",
+            '2.) Click the "ENROLL" button to register for the short course.',
+            "3.) You will be redirected to DragonPay to choose your preferred online payment method (Online Bank, E-Wallet, or Over-the-Counter)."
+        ],
+        cta: "Please feel free to reach out to us for more details or information.☺️"
+    },
+    {
+        question:"How much this short course ?",
+        paragraph:"Good day [Name of the Person]! The training fee for [Name of the short course/ courses] is ₱[amount]. To enroll in any of our short courses, please submit one (1) valid ID and fill out the application form available at the registrar's office.",
+        enroll:"To enroll in our short courses online, please visit the following link: https://courselist.mfi.org.ph/courses/55 (Please change the url base on the course choose)",
+        steps:[
+            "1.) Upon selecting your desired course, kindly scroll down to view the available schedules.",
+            '2.) Click the "ENROLL" button to register for the short course.',
+            "3.) You will be redirected to DragonPay to choose your preferred online payment method (Online Bank, E-Wallet, or Over-the-Counter)."
+        ],
+        cta: "Please feel free to reach out to us for more details or information.☺️"
+    },
+    {
+        question:"What is the age limit of GT Foundation?",
+        paragraph:"Good day [Name of the Person]!, Yes, GT Foundation’s scholarship program has an age limit, which is from 18 to 35 years old. Should you need more information. Feel free to message us if you need further assistance. We're here to help! 🙂"
+    },
+    {
+        question:"What is the location of MFI ?",
+        paragraph:"Good day [Name of the Person]!, We are pleased to inform you of our branch locations:",
+        steps: [
+            "MFI Pasig Branch: MFI Building, Ortigas Avenue, Brgy. Ugong, Pasig City.",
+            "MFI Pasay Branch: Isabel Building, 2240 Taft Avenue, Brgy. 058, Pasay City. Feel free to message us if you need further assistance. We're here to help! 🙂"
+        ],
+        alt: "We are pleased to inform you that our location is at the MFI Pasig Branch, MFI Building, Ortigas Avenue, Brgy. Ugong, Pasig City. Thank you for reaching out! To enroll, please kindly click the link below, and you'll be guided through the process: https://forms.gle/5HJ458TGWmemMdF88 Feel free to message us if you need further assistance. We're here to help! 🙂"
+    },
+    {
+        question:"Ongoing na po ba itong course na ito ?",
+        paragraph:"Good day [Name of the Person]! The [name of the short course/courses] short course will begin on [Date start], until [Date until]. Classes will be held from [Monday to Friday/Saturday]. Please note that all our schedules are tentative, and for some courses, we require a minimum number of participants in order to proceed with the course.",
+        cta:"Feel free to message us if you need further assistance. We're here to help! 🙂"
+    },
+    {
+        question:"Ilan months ang program GT Foundation ?",
+        paragraph:'Good day, [Name of the person]! The program is a 2-year course, consisting of 1 year of academic studies followed by 1 year of On-the-Job Training (OJT). Additionally, there is a 75% allowance provided during the OJT period in GT Foundation.',
+        cta:"Feel free to message us if you need further assistance. We're here to help! 🙂"
+    },
+    {
+        question:'How to enroll GT Foundation?', 
+        paragraph:"[Name of the person] Thank you for reaching out! To enroll, please kindly click the link below, and you'll be guided through the process: https://forms.gle/5HJ458TGWmemMdF88 If you have any questions or need further assistance, feel free to let me know!",
+
+    },
+    {
+        question:'Requirements need in (GT Foundation / DTS )?',
+        paragraph:"Good day [Name of the Person] ! The requirements to be a scholar in GT Foundation are as follows:",
+        steps: [
+            "1.)Good moral character",
+            "2.)Certificate of indigency",
+            "3.)Two (2) 2x2 pictures",
+            "4.)High school education (Form 138 if senior high) or college education (TOR if in college)",
+            "5.) Birth certificate"
+        ],
+        alt:"You can fill out this form: (if GT Foundation [https://forms.gle/5HJ458TGWmemMdF88] else [https://forms.gle/jBD9JL7gv4PAsLd3A]) to ensure your application is processed.",
+        cta:"Please make sure to submit all the necessary documents. Let us know if you need further assistance!"
+    },
+    {
+        question:"Asking a person if there is available this course but don’t have the available the course need?",
+        paragraph:"Good day [Name of the Person] ! Unfortunately, we don’t have the course they specify, Perhaps [Give them alternatives related to that] course, which is related because it involves [explain how it become related]. Let us know if you need more details!"
+    },
+    {
+        question:"They have weekends in TESDA-TWSP",
+        paragraph:"Good day [Name of the Person] ! Unfortunately, we don’t offer weekend classes at TESDA. Our classes run for 7 to 8 hours a day on weekdays.",
+        cta:"Please feel free to reach out to us for more details or information."
+    },
+    {
+        question:"How to enroll to the TESDA-TWSP?",
+        paragraph:'Thank you for your interest! To enroll, please click on the link below and fill out the form. If you need any assistance, feel free to reach out! https://forms.gle/7vJbxqDeWMaraBK26'
+    },
+    {
+        question:"If the course is hybrid (online & face-on-face)",
+        paragraph:'Good day [Name of the Person]!, This course follows a hybrid teaching modality, meaning it includes both online and face-to-face. If you need more details or assistance, feel free to let me know!'
+    },
+    {
+        question:"If we don’t have the weekend class in that course",
+        paragraph:"Good day [Name of the Person]! Unfortunately, there is no schedule available, but we will keep you informed and update you if there is a weekend schedule arranged. Please feel free to reach out to us for more details or information."
+    },
+    {
+        question:"TESDA (no schdule blended or online)",
+        paragraph:'Thank you for your inquiry. Unfortunately, we do not have the exact class schedule for the CSS program at the moment. However, please note that the program is face-to-face, and classes are typically held Monday to Friday. To inquire about any of the TESDA courses, just fill this out: https://forms.gle/7vJbxqDeWMaraBK26.',
+        cta:"Feel free to message us if you need further assistance. We're here to help! 🙂"
+    }
+]
